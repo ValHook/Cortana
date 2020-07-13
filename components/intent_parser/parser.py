bf85f386186b331f4d5abb0278490c7981b2d563
ValHook <vmercier@me.com>
2020-07-13 23:25:48 +0200

    lint parse_activity_type

--- components/intent_parser/parser.py
+++ components/intent_parser/parser.py
@@ -1,29 +1,46 @@
-import difflib
-import math
 import re
 import unidecode
 from protos.activity_id_pb2 import ActivityID
 
 ACTIVITY_NAMES_BY_TYPE = {
-    ActivityID.Type.CALUS: ['leviathan', 'calus'],
-    ActivityID.Type.EATER_OF_WORLDS: ['dévoreur de mondes', 'dévoreur', 'argos', 'mondes'],
-    ActivityID.Type.SPIRE_OF_STARS: ['flèche', 'flèche d\'étoiles', 'étoiles'],
-    ActivityID.Type.CROWN_OF_SORROW: ['couronne', 'couronne du malheur'],
-    ActivityID.Type.LAST_WISH: ['dernier voeu', 'dernier vœu', 'riven', 'voeu', 'vœu',],
-    ActivityID.Type.SCOURGE_OF_THE_PAST: ['fléau', 'fléau du passé'],
-    ActivityID.Type.GARDEN_OF_SALVATION: ['jds', 'jardin', 'jardin du salut'],
-    ActivityID.Type.VAULT_OF_GLASS: ['caveau de verre', 'caveau'],
-    ActivityID.Type.CROPTAS_END: ['la chute de cropta', 'chute de cropta', 'cropta'],
-    ActivityID.Type.THE_TAKEN_KING: ['la chute du roi', 'chute du roi', 'oryx', 'la chute d\'oryx'],
-    ActivityID.Type.WRATH_OF_THE_MACHINE: ['la fureur mécanique', 'fureur mécanique', 'fureur', 'axis'],
+    ActivityID.Type.CALUS:
+        ['leviathan', 'calus'],
+    ActivityID.Type.EATER_OF_WORLDS:
+        ['dévoreur de mondes', 'dévoreur', 'argos', 'mondes'],
+    ActivityID.Type.SPIRE_OF_STARS:
+        ['flèche', 'flèche d\'étoiles', 'étoiles'],
+    ActivityID.Type.CROWN_OF_SORROW:
+        ['couronne', 'couronne du malheur'],
+    ActivityID.Type.LAST_WISH:
+        ['dernier voeu', 'dernier vœu', 'riven', 'voeu', 'vœu', ],
+    ActivityID.Type.SCOURGE_OF_THE_PAST:
+        ['fléau', 'fléau du passé'],
+    ActivityID.Type.GARDEN_OF_SALVATION:
+        ['jds', 'jardin', 'jardin du salut'],
+    ActivityID.Type.VAULT_OF_GLASS:
+        ['caveau de verre', 'caveau'],
+    ActivityID.Type.CROPTAS_END:
+        ['la chute de cropta', 'chute de cropta', 'cropta'],
+    ActivityID.Type.THE_TAKEN_KING:
+        ['la chute du roi', 'chute du roi', 'oryx', 'la chute d\'oryx'],
+    ActivityID.Type.WRATH_OF_THE_MACHINE:
+        ['la fureur mécanique', 'fureur mécanique', 'fureur', 'axis'],
 }
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CALUS_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CALUS]]
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.EATER_OF_WORLDS_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.EATER_OF_WORLDS]]
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.SPIRE_OF_STARS_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.SPIRE_OF_STARS]]
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.VAULT_OF_GLASS_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.VAULT_OF_GLASS]]
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CROPTAS_END_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CROPTAS_END]]
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.THE_TAKEN_KING_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.THE_TAKEN_KING]]
-ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.WRATH_OF_THE_MACHINE_PRESTIGE] = [name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.WRATH_OF_THE_MACHINE]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CALUS_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CALUS]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.EATER_OF_WORLDS_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.EATER_OF_WORLDS]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.SPIRE_OF_STARS_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.SPIRE_OF_STARS]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.VAULT_OF_GLASS_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.VAULT_OF_GLASS]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CROPTAS_END_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.CROPTAS_END]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.THE_TAKEN_KING_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.THE_TAKEN_KING]]
+ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.WRATH_OF_THE_MACHINE_PRESTIGE] = [
+    name + " prestige" for name in ACTIVITY_NAMES_BY_TYPE[ActivityID.Type.WRATH_OF_THE_MACHINE]]
+
 
 class Parser:
     """Parser for user input (intents)."""
@@ -38,7 +55,7 @@ class Parser:
         """Returns the message this parser will process."""
         return self.__message
 
-    def parseActivityType(self, initial_words):
+    def parse_activity_type(self, initial_words):
         """
         Matches an activity type from the given word array.
         Returns (the best matching activity type, the rightmost unused words)
@@ -49,59 +66,52 @@ class Parser:
 
         activities = ACTIVITY_NAMES_BY_TYPE.items()
         best_activity_so_far = None
-        best_levensthein_so_far = 0
         unused_words_for_best_activity_so_far = None
         query = ""
         words = initial_words.copy()
         while len(words) > 0:
-            query += " "+words.pop(0)
-            levensthein_by_activity = map(lambda a: (a[0], min([self.levensthein(query, name) for name in a[1]])), activities)
+            query += " " + words.pop(0)
+            levensthein_by_activity = map(lambda a: (a[0], min(
+                [self.levensthein(query, name) for name in a[1]])), activities)
             levensthein_by_activity = list(levensthein_by_activity)
-            levensthein_by_activity = sorted(levensthein_by_activity, key=lambda a: a[1])
+            levensthein_by_activity = sorted(
+                levensthein_by_activity, key=lambda a: a[1])
             (best_activity, best_levensthein) = levensthein_by_activity[0]
 
-            if best_levensthein > 3 or best_levensthein > 0.3*len(query):
+            if best_levensthein > 3 or best_levensthein > 0.3 * len(query):
                 continue
 
-            #print()
-            #print(initial_words)
-            #print(query)
-            #print(levensthein_by_activity)            
             best_activity_so_far = best_activity
-            best_levensthein_so_far = best_levensthein
             unused_words_for_best_activity_so_far = words.copy()
-            
-        
+
         if not best_activity_so_far:
-            raise ValueError("Un nom d'activité aurait dû être présent à partir de \""+" ".join(initial_words)+"\"")
+            raise ValueError(
+                "Un nom d'activité aurait dû être présent à partir de \"" +
+                " ".join(initial_words) +
+                "\"")
 
         return (best_activity_so_far, unused_words_for_best_activity_so_far)
 
-
-    def levensthein(self, s1, s2):
-        """Returns a slightly adjusted levensthein distance between s1 and s2 that"""
-        s1 = unidecode.unidecode(s1)
-        s2 = unidecode.unidecode(s2)
-        s1 = s1.strip()
-        s2 = s2.strip()
-        s1 = s1.lower()
-        s2 = s2.lower()
-        s1 = re.sub('[^A-Za-z0-9+-]+', '', s1)
-        s2 = re.sub('[^A-Za-z0-9+-]+', '', s2)
-        if len(s1) > len(s2):
-            s1, s2 = s2, s1
-        distances = range(len(s1) + 1)
-        for i2, c2 in enumerate(s2):
-            distances_ = [i2+1]
-            for i1, c1 in enumerate(s1):
+    def levensthein(self, str_a, str_b):
+        """Returns a slightly adjusted levensthein distance between str_a and str_b that"""
+        str_a = unidecode.unidecode(str_a)
+        str_b = unidecode.unidecode(str_b)
+        str_a = str_a.strip()
+        str_b = str_b.strip()
+        str_a = str_a.lower()
+        str_b = str_b.lower()
+        str_a = re.sub('[^A-Za-z0-9+-]+', '', str_a)
+        str_b = re.sub('[^A-Za-z0-9+-]+', '', str_b)
+        if len(str_a) > len(str_b):
+            str_a, str_b = str_b, str_a
+        distances = range(len(str_a) + 1)
+        for i2, c2 in enumerate(str_b):
+            distances_ = [i2 + 1]
+            for i1, c1 in enumerate(str_a):
                 if c1 == c2:
                     distances_.append(distances[i1])
                 else:
-                    distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+                    distances_.append(
+                        1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
             distances = distances_
         return distances[-1]
-                
-                
-            
-        
-        
--- components/intent_parser/parser_test.py
+++ components/intent_parser/parser_test.py
@@ -18,41 +18,66 @@ class ParserTest(unittest.TestCase):
         """Verifies the activity-type sub-parser."""
         sut = parser.Parser('query does not matter')
         expectations = {
-            ActivityID.Type.CALUS: ['leviathan', 'calus', 'ckalus', 'leviatan', 'leviath', 'caluss'],
-            ActivityID.Type.CALUS_PRESTIGE: ['leviathan prestige', 'calus prestige', 'cal prestige', 'leviath prestig'],
-            ActivityID.Type.EATER_OF_WORLDS: ['dévoreur de mondes', 'dévoreur', 'argos', 'dévore', 'argo', 'monde'],
-            ActivityID.Type.EATER_OF_WORLDS_PRESTIGE: ['dévoreur de mondes prestige', 'dévoreur prestige', 'argos prestige', 'dévor prestige'],
-            ActivityID.Type.SPIRE_OF_STARS: ['flèche', 'flèche d\'étoiles', 'fleche', 'flech', 'etoile', 'flechedetoiles', 'detoiles'],
-            ActivityID.Type.SPIRE_OF_STARS_PRESTIGE: ['flèche prestige', 'flèche d\'étoiles prestige', 'fleche prestige', 'etoile prestige', 'flèchedetoiles prestige'],
-            ActivityID.Type.CROWN_OF_SORROW: ['couronne', 'couronne du malheur', 'couronnedumalheur', 'kouronn'],
-            ActivityID.Type.LAST_WISH: ['dernier voeu', 'dernier vœu', 'riven', 'voeu', 'vœu', 'rive', 'rivem'],
-            ActivityID.Type.SCOURGE_OF_THE_PAST: ['fléau', 'fléau du passé', 'fleau', 'fleaudupasse', 'fleaupasse'],
-            ActivityID.Type.GARDEN_OF_SALVATION: ['jds', 'jardin', 'jardin du salut', 'gds', 'jardim', 'jardindusalu'],
-            ActivityID.Type.VAULT_OF_GLASS: ['caveau', 'caveauverre', 'caveau de verre'],
-            ActivityID.Type.VAULT_OF_GLASS_PRESTIGE: ['caveauprestige'],
-            ActivityID.Type.CROPTAS_END: ['cropta', 'chutecropta', 'cropto'],
-            ActivityID.Type.CROPTAS_END_PRESTIGE: ['cropta prestige', 'crotpa prestige', 'cropto presti'],
-            ActivityID.Type.THE_TAKEN_KING: ['oryx', 'chuteroi', 'chuteoryx', 'orix'],
-            ActivityID.Type.THE_TAKEN_KING_PRESTIGE: ['oryx prestige', 'orix presti', 'oryxprest', 'chuteroiprestige'],
-            ActivityID.Type.WRATH_OF_THE_MACHINE: ['fureur', 'axis', 'furer', 'fureurmecanique', 'axos'],
-            ActivityID.Type.WRATH_OF_THE_MACHINE_PRESTIGE: ['axis prestige', 'fureur prest', 'axs prestig', 'axos prestige'],
+            ActivityID.Type.CALUS:
+                ['leviathan', 'calus', 'ckalus', 'leviatan', 'leviath', 'caluss'],
+            ActivityID.Type.CALUS_PRESTIGE:
+                ['leviathan prestige', 'calus prestige', 'cal prestige', 'leviath prestig'],
+            ActivityID.Type.EATER_OF_WORLDS:
+                ['dévoreur de mondes', 'dévoreur', 'argos', 'dévore', 'argo', 'monde'],
+            ActivityID.Type.EATER_OF_WORLDS_PRESTIGE:
+                ['dévoreur de mondes prestige', 'dévoreur prestige', 'argos prestige',
+                 'dévor prestige'],
+            ActivityID.Type.SPIRE_OF_STARS:
+                ['flèche', 'flèche d\'étoiles', 'fleche', 'flech', 'etoile', 'flechedetoiles',
+                 'detoiles'],
+            ActivityID.Type.SPIRE_OF_STARS_PRESTIGE:
+                ['flèche prestige', 'flèche d\'étoiles prestige', 'fleche prestige',
+                 'etoile prestige', 'flèchedetoiles prestige'],
+            ActivityID.Type.CROWN_OF_SORROW:
+                ['couronne', 'couronne du malheur', 'couronnedumalheur', 'kouronn'],
+            ActivityID.Type.LAST_WISH:
+                ['dernier voeu', 'dernier vœu', 'riven', 'voeu', 'vœu', 'rive', 'rivem'],
+            ActivityID.Type.SCOURGE_OF_THE_PAST:
+                ['fléau', 'fléau du passé', 'fleau', 'fleaudupasse', 'fleaupasse'],
+            ActivityID.Type.GARDEN_OF_SALVATION:
+                ['jds', 'jardin', 'jardin du salut', 'gds', 'jardim', 'jardindusalu'],
+            ActivityID.Type.VAULT_OF_GLASS:
+                ['caveau', 'caveauverre', 'caveau de verre'],
+            ActivityID.Type.VAULT_OF_GLASS_PRESTIGE:
+                ['caveauprestige'],
+            ActivityID.Type.CROPTAS_END:
+                ['cropta', 'chutecropta', 'cropto'],
+            ActivityID.Type.CROPTAS_END_PRESTIGE:
+                ['cropta prestige', 'crotpa prestige', 'cropto presti'],
+            ActivityID.Type.THE_TAKEN_KING:
+                ['oryx', 'chuteroi', 'chuteoryx', 'orix'],
+            ActivityID.Type.THE_TAKEN_KING_PRESTIGE:
+                ['oryx prestige', 'orix presti', 'oryxprest', 'chuteroiprestige'],
+            ActivityID.Type.WRATH_OF_THE_MACHINE:
+                ['fureur', 'axis', 'furer', 'fureurmecanique', 'axos'],
+            ActivityID.Type.WRATH_OF_THE_MACHINE_PRESTIGE:
+                ['axis prestige', 'fureur prest', 'axs prestig', 'axos prestige'],
         }
-        noises = ["", "croptus", "Franstuk Walnut Oby1Chick 25/07", "+Cosa58 25/07", "-SuperFayaChonch", "+emile -Omega Gips"]
+        noises = ["", "croptus", "Franstuk Walnut Oby1Chick 25/07",
+                  "+Cosa58 25/07", "-SuperFayaChonch", "+emile -Omega Gips"]
         for (activity_type, candidates) in expectations.items():
             for candidate in candidates:
                 for noise in noises:
                     candidate_array = re.split(r"\s+", candidate)
                     noise_array = list(filter(len, re.split(r"\s+", noise)))
                     candidate_and_noise_array = candidate_array + noise_array
-                    debug_str = " ".join(candidate_and_noise_array) + " => " + ActivityID.Type.Name(activity_type)
-                    result = sut.parseActivityType(candidate_and_noise_array)
+                    debug_str = " ".join(
+                        candidate_and_noise_array) + " => " + ActivityID.Type.Name(activity_type)
+                    result = sut.parse_activity_type(candidate_and_noise_array)
                     self.assertEqual(activity_type, result[0], debug_str)
                     self.assertEqual(noise_array, result[1], debug_str)
 
-        noises = ["backup", "save finish", "info", "foo", "noise", "Walnut Waffle", "BAB", "Dark01light", "raid", "",]
+        noises = ["backup", "save finish", "info", "foo", "noise", "Walnut Waffle", "BAB",
+                  "Dark01light", "raid", ""]
         for noise in noises:
             noise_array = list(filter(len, re.split(r"\s+", noise)))
-            self.assertRaises(ValueError, sut.parseActivityType, noise_array)
+            self.assertRaises(ValueError, sut.parse_activity_type, noise_array)
+
 
 if __name__ == '__main__':
     unittest.main()

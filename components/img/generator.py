48a3fe3f1137d3ebc71f92bbcbabd857a4a75fb3
Benoit Chabod <bchabod@palantir.com>
2020-07-23 11:43:32 +0200

    Improve image generation (multi-section, colors, nits)

--- components/img/generator.py
+++ components/img/generator.py
@@ -1,15 +1,16 @@
-from protos.planning_pb2 import Planning
 from protos.activity_id_pb2 import ActivityID
+from protos.rated_player_pb2 import RatedPlayer
 from PIL import Image, ImageDraw, ImageSequence, ImageFont
 import io
-import locale
+import tempfile
 from datetime import datetime
+from babel.dates import format_datetime
 
-locale.setlocale(locale.LC_TIME, "fr_FR")
-
+# Coordinates on the image as deltas to (X=0, Y=0) for the 1st banner on the GIF
+# Name is short to keep the logic compact in methods below
 COORDS = {
-    "activity": (170, 20),
-    "details": (140, 40),
+    "activity": (20, 20),
+    "details": (20, 40),
     "players": [
         (30, 60),
         (30, 85),
@@ -22,11 +23,14 @@ COORDS = {
         (220, 148),
         (220, 168),
         (220, 188)
-    ]
+    ],
+    "section_w": 350,
+    "delta_x": 486,
+    "delta_y": 260,
 }
 
 ACTIVITY_NAMES = {
-    ActivityID.Type.CALUS: "Calus",
+    ActivityID.Type.LEVIATHAN: "Léviathan",
     ActivityID.Type.EATER_OF_WORLDS: "Dévoreur de Mondes",
     ActivityID.Type.SPIRE_OF_STARS: "Flèche d'étoiles",
     ActivityID.Type.CROWN_OF_SORROW: "Couronne du Malheur",
@@ -39,36 +43,121 @@ ACTIVITY_NAMES = {
     ActivityID.Type.WRATH_OF_THE_MACHINE: "Fureur Mécanique",
 }
 
+# Add Prestige names when needed
+for activity_id in list(ACTIVITY_NAMES):
+    name_prestige = ActivityID.Type.Name(activity_id) + "_PRESTIGE"
+    if name_prestige in ActivityID.Type.keys():
+        prestige_id = ActivityID.Type.Value(name_prestige)
+        ACTIVITY_NAMES[prestige_id] = ACTIVITY_NAMES[activity_id] + " - Prestige"
+
 CR = ImageFont.truetype('components/img/assets/calibri_bold.ttf', 16)
 CI = ImageFont.truetype('components/img/assets/calibri_italic.ttf', 16)
 
+COLORS = {
+    "blue": (78, 96, 224, 255),
+    "purple": (144, 9, 226, 255),
+    "black": (0, 0, 0, 255),
+    "green": (15, 155, 73, 255),
+    "orange": (188, 96, 15, 255),
+}
+
+COLORS_BY_RATING = {
+    RatedPlayer.Rating.UNKNOWN: COLORS["black"],
+    RatedPlayer.Rating.BEGINNER: COLORS["green"],
+    RatedPlayer.Rating.INTERMEDIATE: COLORS["orange"],
+    RatedPlayer.Rating.EXPERIENCED: COLORS["blue"],
+}
+
 class Generator:
-    """Generates the planning in GIF/PNG format."""
+    """Generates the planning in GIF format."""
 
     def __init__(self, planning):
         self.__planning = planning
 
-    def write_to_frame(self, f):
-        a = self.__planning.activities[0]
-        f.text(COORDS["activity"], ACTIVITY_NAMES[a.id.type], font=CR, fill=(144,9,226,255))
-        sdate = datetime.utcfromtimestamp(a.id.timestamp_seconds).strftime("%A %d %B")
-        f.text(COORDS["details"], sdate, font=CI, fill=(144,9,226,255))
-        for i, p in enumerate(a.squad.players):
-            f.text(COORDS["players"][i], p.gamer_tag, font=CR, fill=(78,96,224,255))
-        f.text(COORDS["substitutes"][0], "Remplaçants:", font=CR, fill=(0,0,0,255))
-        for i, p in enumerate(a.squad.substitutes):
-            f.text(COORDS["substitutes"][i+1], p.gamer_tag, font=CR, fill=(78,96,224,255))
+    def move(self, coordinates, section, width_to_center=None):
+        """
+        Moves coordinates at the right place depending on the section and the center preferences.
+        :param coordinates: tuple (X,Y) representing the initial coordinates
+        :param section: the section index, e.g. from 0 to 3 for the 1st GIF
+        :param width_to_center: optional parameter indicating the width of message if it has to be centered
+        :return: tuple for the new coordinates
+        """
+        dx = COORDS["delta_x"] if (section % 2 == 1) else 0
+        dy = COORDS["delta_y"] if ((section / 4.0) % 1 >= 0.5) else 0
+        new_x = coordinates[0] + dx
+        new_y = coordinates[1] + dy
+        if width_to_center:
+            new_x += (COORDS["section_w"] - width_to_center) / 2
+        return (new_x, new_y)
+
+    def write_to_frame(self, frame, banner_number):
+        """
+        Writes the relevant information for a given GIF frame.
+        :param frame: an Image object to write to
+        :param banner_number: an int representing the GIF index
+        """
+        for section in range(banner_number * 4, min(banner_number * 4 + 4, len(self.__planning.activities))):
+            a = self.__planning.activities[section]
+            name_w = CR.getsize(ACTIVITY_NAMES[a.id.type])[0]
+            frame.text(
+                self.move(COORDS["activity"], section, name_w),
+                ACTIVITY_NAMES[a.id.type], font=CR,
+                fill=COLORS["purple"]
+            )
+            sdate = format_datetime(
+                datetime.utcfromtimestamp(a.id.timestamp_seconds),
+                format="EEEE d MMMM, à HH:mm",
+                locale="fr"
+            ).capitalize()
+            detail_w = CR.getsize(sdate)[0]
+            frame.text(
+                self.move(COORDS["details"], section, detail_w),
+                sdate,
+                font=CI,
+                fill=COLORS["purple"]
+            )
+            for i, p in enumerate(a.squad.players):
+                frame.text(
+                    self.move(COORDS["players"][i], section),
+                    p.gamer_tag,
+                    font=CR,
+                    fill=COLORS_BY_RATING[p.rating]
+                )
+            frame.text(
+                self.move(COORDS["substitutes"][0], section),
+                "Remplaçants:",
+                font=CR,
+                fill=COLORS["black"]
+            )
+            for i, p in enumerate(a.squad.substitutes):
+                frame.text(
+                    self.move(COORDS["substitutes"][i+1], section),
+                    p.gamer_tag,
+                    font=CR,
+                    fill=COLORS_BY_RATING[p.rating]
+                )
 
     def generate_image(self):
-        with Image.open('components/img/assets/empty_banner.gif') as im:
-            frames = []
-            for frame in ImageSequence.Iterator(im):
-                frame = frame.convert('RGB')
-                d = ImageDraw.Draw(frame)
-                self.write_to_frame(d)
-                del d
-                b = io.BytesIO()
-                frame.save(b, format="GIF")
-                frame = Image.open(b)
-                frames.append(frame)
-            frames[0].save('/tmp/out.gif', save_all=True, append_images=frames[1:])
+        """
+        Generates as many images as needed to display the activity planning.
+        GIFs are stored as temporary files on disk.
+        :return: an array of paths
+        """
+        gifs = []
+        needed_banners = ((len(self.__planning.activities) - 1) // 4) + 1
+        for banner_number in range(needed_banners):
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.gif') as temp:
+                with Image.open('components/img/assets/empty_banner.gif') as im:
+                    frames = []
+                    for frame in ImageSequence.Iterator(im):
+                        frame = frame.convert('RGB')
+                        d = ImageDraw.Draw(frame)
+                        self.write_to_frame(d, banner_number)
+                        del d
+                        b = io.BytesIO()
+                        frame.save(b, format="GIF")
+                        frame = Image.open(b)
+                        frames.append(frame)
+                    frames[0].save(temp.name, save_all=True, append_images=frames[1:])
+                    gifs.append(temp.name)
+        return gifs
--- components/img/generator_test.py
+++ components/img/generator_test.py
@@ -2,25 +2,93 @@ import unittest
 from components.img import generator
 from protos.planning_pb2 import Planning
 from protos.activity_id_pb2 import ActivityID
+from protos.rated_player_pb2 import RatedPlayer
 
 class GeneratorTester(unittest.TestCase):
     """Test class for the image generator."""
 
     def test_planning(self):
+        """Not a real test, but writes a sample image on disk."""
         p = Planning()
         a = p.activities.add()
-        a.id.type = ActivityID.Type.CALUS
+        a.id.type = ActivityID.Type.LEVIATHAN
         a.id.timestamp_seconds = 1594763746
         (a.squad.players.add()).gamer_tag = "Cosa58"
+        a.squad.players[-1].rating = RatedPlayer.Rating.EXPERIENCED
         (a.squad.players.add()).gamer_tag = "Walnut Waffle"
+        a.squad.players[-1].rating = RatedPlayer.Rating.EXPERIENCED
         (a.squad.players.add()).gamer_tag = "Oby1Chick"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
         (a.squad.players.add()).gamer_tag = "Franstuck"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
         (a.squad.players.add()).gamer_tag = "Dark0l1ght"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
         (a.squad.players.add()).gamer_tag = "SuperFayaChonch"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
         (a.squad.substitutes.add()).gamer_tag = "croptus7490"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.BEGINNER
         (a.squad.substitutes.add()).gamer_tag = "Affectevil"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.BEGINNER
+        a = p.activities.add()
+        a.id.type = ActivityID.Type.SPIRE_OF_STARS_PRESTIGE
+        a.id.timestamp_seconds = 1595493712
+        (a.squad.players.add()).gamer_tag = "Cosa58"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "Walnut Waffle"
+        a.squad.players[-1].rating = RatedPlayer.Rating.EXPERIENCED
+        (a.squad.players.add()).gamer_tag = "Oby1Chick"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        (a.squad.players.add()).gamer_tag = "Franstuck"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "Dark0l1ght"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "SuperFayaChonch"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        (a.squad.substitutes.add()).gamer_tag = "croptus7490"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.substitutes.add()).gamer_tag = "Affectevil"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        a = p.activities.add()
+        a.id.type = ActivityID.Type.SCOURGE_OF_THE_PAST
+        a.id.timestamp_seconds = 1603639859
+        (a.squad.players.add()).gamer_tag = "Cosa58"
+        a.squad.players[-1].rating = RatedPlayer.Rating.UNKNOWN
+        (a.squad.players.add()).gamer_tag = "Walnut Waffle"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "Oby1Chick"
+        a.squad.players[-1].rating = RatedPlayer.Rating.EXPERIENCED
+        (a.squad.players.add()).gamer_tag = "Franstuck"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        (a.squad.players.add()).gamer_tag = "Dark0l1ght"
+        a.squad.players[-1].rating = RatedPlayer.Rating.EXPERIENCED
+        (a.squad.players.add()).gamer_tag = "SuperFayaChonch"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        (a.squad.substitutes.add()).gamer_tag = "croptus7490"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.substitutes.add()).gamer_tag = "Affectevil"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        a = p.activities.add()
+        a.id.type = ActivityID.Type.GARDEN_OF_SALVATION
+        a.id.timestamp_seconds = 1598387759
+        (a.squad.players.add()).gamer_tag = "Cosa58"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        (a.squad.players.add()).gamer_tag = "Walnut Waffle"
+        a.squad.players[-1].rating = RatedPlayer.Rating.INTERMEDIATE
+        (a.squad.players.add()).gamer_tag = "Oby1Chick"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "Franstuck"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "Dark0l1ght"
+        a.squad.players[-1].rating = RatedPlayer.Rating.BEGINNER
+        (a.squad.players.add()).gamer_tag = "SuperFayaChonch"
+        a.squad.players[-1].rating = RatedPlayer.Rating.EXPERIENCED
+        (a.squad.substitutes.add()).gamer_tag = "croptus7490"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.EXPERIENCED
+        (a.squad.substitutes.add()).gamer_tag = "Affectevil"
+        a.squad.substitutes[-1].rating = RatedPlayer.Rating.BEGINNER
         gen = generator.Generator(p)
-        gen.generate_image()
+        gifs = gen.generate_image()
+        print("IMPORTANT - the result of this test can be visualized here: ", gifs)
         self.assertEqual(True, True)
 
 if __name__ == '__main__':
